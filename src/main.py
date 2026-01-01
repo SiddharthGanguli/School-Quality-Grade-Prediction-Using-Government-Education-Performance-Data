aa079b0be1e2b0f7a732cbcdea7483c4e301b0c7
Siddhartha Ganguli <siddharthaganguli0093@gmail.com>
2026-01-01 17:34:25 +0530

    adding spliting scrept in main.py pipeline

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 from preprocessing import PreprocessingData
+from split import SplitData
 
 def main():
-    input_path = "/Users/siddharthaganguli/Desktop/Goverment/Data/Raw/datafile.csv"
-    output_path = "/Users/siddharthaganguli/Desktop/Goverment/Data/Processed/processed_data.csv"
+    raw_data_path = "/Users/siddharthaganguli/Desktop/Goverment/Data/Raw/datafile.csv"
+    processed_data_path = "/Users/siddharthaganguli/Desktop/Goverment/Data/Processed/processed_data.csv"
+    split_output_dir = "/Users/siddharthaganguli/Desktop/Goverment/Data/Splits"
 
-    preprocess = PreprocessingData(
-        input_path=input_path,
-        output_path=output_path
-    )
-
+    preprocess = PreprocessingData(raw_data_path, processed_data_path)
     preprocess.run_preprocessing()
-    print("Data preprocessing completed...")
+
+    split = SplitData(
+        data_path=processed_data_path,
+        output_dir=split_output_dir
+    )
+    split.run_split()
+
+    print("Data split into x_train and y_train.")
 
 if __name__ == "__main__":
     main()
