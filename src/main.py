65b99f4110888a3fcd5a0228a2809b33ff12c194
Siddhartha Ganguli <siddharthaganguli0093@gmail.com>
2026-01-01 17:45:06 +0530

    adding traning scrept in main.py pipeline

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from preprocessing import PreprocessingData
 from split import SplitData
+from train import TrainModel
 
 def main():
     raw_data_path = "/Users/siddharthaganguli/Desktop/Goverment/Data/Raw/datafile.csv"
     processed_data_path = "/Users/siddharthaganguli/Desktop/Goverment/Data/Processed/processed_data.csv"
     split_output_dir = "/Users/siddharthaganguli/Desktop/Goverment/Data/Splits"
+    model_path = "/Users/siddharthaganguli/Desktop/Goverment/model/grade_model.pkl"
 
     preprocess = PreprocessingData(raw_data_path, processed_data_path)
     preprocess.run_preprocessing()
@@ -17,5 +19,9 @@ def main():
 
     print("Data split into x_train and y_train.")
 
+
+    trainer = TrainModel(split_output_dir, model_path)
+    trainer.run_training()
+    
 if __name__ == "__main__":
     main()
