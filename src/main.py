1f906bcd8c4b7c6445634a641bdd6bb222dd6bbe
Siddhartha Ganguli <siddharthaganguli0093@gmail.com>
2026-01-01 18:21:43 +0530

    Removing extra data saving folder

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from preprocessing import PreprocessingData
 from split import SplitData
 from train import TrainModel
+from SMOTE_data import ApplySMOTE
 
 def main():
     raw_data_path = "/Users/siddharthaganguli/Desktop/Goverment/Data/Raw/datafile.csv"
     processed_data_path = "/Users/siddharthaganguli/Desktop/Goverment/Data/Processed/processed_data.csv"
     split_output_dir = "/Users/siddharthaganguli/Desktop/Goverment/Data/Splits"
     model_path = "/Users/siddharthaganguli/Desktop/Goverment/model/grade_model.pkl"
+    smote_split="/Users/siddharthaganguli/Desktop/Goverment/Data/Smote"
 
     preprocess = PreprocessingData(raw_data_path, processed_data_path)
     preprocess.run_preprocessing()
@@ -20,8 +22,19 @@ def main():
     print("Data split into x_train and y_train.")
 
 
-    trainer = TrainModel(split_output_dir, model_path,experiment_name="School_Grade_Prediction")
-    trainer.run_training()
+    smote_data=ApplySMOTE(
+        split_dir=split_output_dir,
+        output_dir=split_output_dir
+
+    )
+    smote_data.run_smote()
+
+    # trainer = TrainModel(split_output_dir, model_path,experiment_name="School_Grade_Prediction")
+    # trainer.run_training()
+
+    # trainer = TrainModel(split_output_dir, model_path,experiment_name="SmoteSchool_Grade_Prediction")
+    # trainer.run_training()
+    
 
 if __name__ == "__main__":
     main()
